bad7efd8b39eb2324d8a01157d92f92a65436909
vincent zhang <vincent.zhang@edreamsodigeo.com>
2022-03-28 22:18:38 +0200

    add consul and zookeeper

---
+++ cloud-consumerconsul-order9003/src/main/java/com/atguigu/springcloud/controller/OrderConsulController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderConsulController
+{
+
+    @Value("${application.payment-service-url}")
+    private String serviceURL;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/consul")
+    public String paymentInfo()
+    {
+        String result = restTemplate.getForObject(serviceURL+"/payment/consul",String.class);
+        return result;
+    }
+}
